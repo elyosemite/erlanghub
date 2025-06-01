87b5a105cfbb1cd46627c2eb5863ebecdf71eaa1
Yuri Melo <yurifullstack@gmail.com>
2025-06-01 11:43:58 -0300

    Add siples Erlang functions

--- src/hello_world.erl
+++ src/hello_world.erl
@@ -1,6 +1,8 @@
 %% src/hello_world.erl
 -module(hello_world).
--export([say/0]).
+
+-export([say/0, my_age/0, my_name/0, process_credit_card_payment/0,
+         process_debit_card_payment/0, my_favorite_colors/0]).
 
 say() ->
     io:format("Hello, World!~n"),
@@ -8,3 +10,18 @@ say() ->
 
 second() ->
     io:format("Hello from Erlang!~n").
+
+my_age() ->
+    27.
+
+my_name() ->
+    "El Yosemite".
+
+process_credit_card_payment() ->
+    {ok, "Success"}.
+
+process_debit_card_payment() ->
+    {fail, "You don't have limit"}.
+
+my_favorite_colors() ->
+    ["blue", "green", "white", "black"].
